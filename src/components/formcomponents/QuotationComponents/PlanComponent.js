2721704a02eaeb76e13af687512c01e042739ad4
taufiqzaidong <58703430+taufiqzaidong@users.noreply.github.com>
2021-06-07 17:15:53 +0800

    choose plan ui update topek
    alhamdulillah

--- src/components/formcomponents/QuotationComponents/PlanComponent.js
+++ src/components/formcomponents/QuotationComponents/PlanComponent.js
@@ -53,36 +53,37 @@ class PlanComponent extends Component {
 
         
         return (
-            <div style={{'backgroundColor': backgroundColor}} className={"individual-plan"} >
-                <div  style={{'backgroundColor': '#7030A0','color': '#FFFFFF'}}className={'plan-header'}>{`eMedic ${this.props.plan.name}`} </div>
-                <div className={'plan-price'}>RM {this.props.basePriceMonthly}<span>/month</span></div>
-               <div style={{fontSize: 15}} className={'plan-annual-limit'}>
-                    with {`RM${this.props.plan["annual_limit"].toLocaleString()}`} annual limit
+            <div className={"individual-plan"} style={{'backgroundColor': backgroundColor}}>
+                <div  className={'plan-header'} style={{'color': '#FFFFFF'}}>{`eMedic ${this.props.plan.name}`} </div>
+            
+                <div className={'plan-price'} style={{'backgroundColor': '#FBBBB4'}}>RM {this.props.basePriceMonthly}<span>/month</span></div>
+                <div className={'plan-annual-limit'} style={{fontSize: 15}} >
+                        with {`RM${this.props.plan["annual_limit"].toLocaleString()}`} annual limit
+                    </div>
+                    <div className={'plan-info'} style={{fontSize: 13}}>
+                        <ul className='plan-description'>
+                            <li>
+                                <span>
+                                    Cover for hospitalisation expenses{" "}
+                                    <strong>in Malaysia only,</strong> up to
+                                    RM{` ${this.props.plan["annual_limit"].toLocaleString()}`}, including surgery, ambulance, ICU and
+                                    prescription drugs<br/><br/>
+                                </span>
+                            </li>
+                            <li>
+                                Cashless admission to <a href="https://www.axa.com.my/hospital-panel-life" target="_blank">AXA panel hospitals</a><br/><br/>
+                            </li>
+                            <li>
+                                Cover for room and board up to RM250 a day (choose any room type that is available within this budget)
+                            </li>
+                        </ul>
+                </div><br/>
+                <div>
+                    <button onClick={this.handleOnClick} className="choose-plan-btn" >Choose This Plan   ⪢</button>
+                    {userForm}
                 </div>
-                <div style={{fontSize: 13}}   className={'plan-info'}>
-                    <ul className='plan-description'>
-                        <li>
-                            
-                            <span>
-                                Cover for hospitalisation expenses{" "}
-                                <strong>in Malaysia only,</strong> up to
-                                RM{` ${this.props.plan["annual_limit"].toLocaleString()}`}, including surgery, ambulance, ICU and
-                                prescription drugs<br/><br/>
-                            </span>
-                        </li>
-                        <li>
-                            Cashless admission to <a href="https://www.axa.com.my/hospital-panel-life" target="_blank">AXA panel hospitals</a><br/><br/>
-                        </li>
-                        <li>
-                            Cover for room and board up to RM250 a day (choose any room type that is available within this budget)
-                        </li>
-                    </ul>
-                </div>
-                
-                
-                <button onClick={this.handleOnClick} className="choose-plan-btn" >Choose This Plan   ⪢</button>
-                {userForm}
             </div>
+            
         );
     }
 }
